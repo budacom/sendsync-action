9b66820f233daee95f884809456b8a10db9fa485
mauricepoirrier <mauricepoirrier@gmail.com>
2021-05-07 17:51:15 -0400

    feat: create pr's

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,21 @@ RUN go mod download
 RUN go mod verify
 RUN CGO_ENABLED=0 go build -o sendsync
 
+FROM alpine:latest as gh
+
+RUN apk --no-cache add wget tar
+RUN wget https://github.com/cli/cli/releases/download/v1.9.2/gh_1.9.2_linux_amd64.tar.gz
+RUN tar -zxvf gh_1.9.2_linux_amd64.tar.gz
+RUN chmod a+x gh_1.9.2_linux_amd64/bin/gh
+
 FROM alpine:latest
 
 RUN apk --no-cache add ca-certificates git
 
 WORKDIR /root/
+
+RUN mkdir /lib64 && ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
+COPY --from=gh gh_1.9.2_linux_amd64/bin/gh /usr/bin/gh
 
 COPY --from=build /go/src/github.com/budacom/sendsync/sendsync /usr/local/bin
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,33 @@
 #!/bin/sh -l
 
-sendsync get template
+DRY_RUN="$1"
 
-git status templates/
+git config --global user.email "devops@buda.com"
+git config --global user.name "Buda CD"
+git config pull.rebase true
+git fetch origin
+git pull origin
+
+sendsync get template
 
 templates=$(git status -s templates | cut -d' ' -f3 | cut -d'/' -f 2 | tr '\n' ',' | sed 's/,$//g')
 
-echo "::set-output name=templates::${templates}"
+git stash
+
+for template in $(echo ${templates} | sed 's/,/ /g'); do
+    if [ $DRY_RUN="false" ]; then 
+        git branch ${template}
+        git checkout ${template}
+        git pull origin ${template}
+        git stash apply
+        git add templates/${template}
+        git commit -m "(auto) Changes in ${template}"
+        git push origin ${template}
+        echo ${GITHUB_TOKEN} | gh auth login --with-token
+        gh pr create -d --title "(auto) Publish template: ${template}." --body "Detected changes in template ${template}. Review this PR to approve."
+        git stash
+        git checkout master
+    else 
+        echo "DRY RUN: would have created PR for ${template}"
+    fi
+done
