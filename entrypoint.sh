8d4fedcf357e4bcafa0801dbcbf8ba50d58a0189
mauricepoirrier <mauricepoirrier@gmail.com>
2021-05-07 16:25:54 -0400

    mend

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
-git status
-ls
 sendsync get template
 
-templates=$(git diff-files --name-only templates/ | cut -d '/' -f 2 | tr '\n' ',' | sed 's/,$//g')
+git status templates/
 
-echo "::set-output name=templates::$templates"
+templates=$(git status -s templates | cut -d' ' -f3 | cut -d'/' -f 2 | tr '\n' ',' | sed 's/,$//g')
+
+echo "::set-output name=templates::${templates}"
