25c2052f6b69250d036e6981340c0b327a662afc
Arnaldo Garat <arnaldo.garat@buda.com>
2021-05-10 16:03:38 -0400

    feat: adding apply to sendsync action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,54 @@
 #!/bin/sh -l
 
-DRY_RUN="$1"
+set -x
 
+DRY_RUN="$1"
+MODE="$2"
+
+alias tpl_path_join="cut -d'/' -f 2 | sort | uniq"
 git config --global user.email "devops@buda.com"
 git config --global user.name "Buda CD"
 git config pull.rebase true
 git fetch origin
-git pull origin
+git merge origin/master
 
-sendsync get template
+if [ $MODE = "sync" ]; then
 
-templates=$(git status -s templates | cut -d' ' -f3 | cut -d'/' -f 2 | tr '\n' ',' | sed 's/,$//g')
+    sendsync get template
 
-git stash
+    templates=$(git status -s templates | cut -d' ' -f3 | tpl_path_join)
 
-for template in $(echo ${templates} | sed 's/,/ /g'); do
-    if [ $DRY_RUN="false" ]; then 
-        git branch ${template}
-        git checkout ${template}
-        git pull origin ${template}
-        git stash apply
-        git add templates/${template}
-        git commit -m "(auto) Changes in ${template}"
-        git push origin ${template}
-        echo ${GITHUB_TOKEN} | gh auth login --with-token
-        gh pr create -d --title "(auto) Publish template: ${template}." --body "Detected changes in template ${template}. Review this PR to approve."
-        git stash
-        git checkout master
-    else 
-        echo "DRY RUN: would have created PR for ${template}"
-    fi
-done
+    git stash
+
+    for template in $(echo ${templates}); do
+        if [ $DRY_RUN = "false" ]; then 
+            git branch ${template}
+            git checkout ${template}
+            git pull origin ${template}
+            git stash apply
+            git add templates/${template}
+            git commit -m "(auto) Changes in ${template}"
+            git push origin ${template}
+            echo ${GITHUB_TOKEN} | gh auth login --with-token
+            gh pr create -d --title "(auto) Publish template: ${template}." --body "Detected changes in template ${template}. Review this PR to approve."
+            git stash
+            git checkout master
+        else 
+            echo "DRY RUN: would have created PR for ${template}"
+        fi
+    done
+elif [ $MODE = "apply" ]; then
+    git pull origin master
+    git diff --name-only ${GITHUB_SHA} HEAD~1 templates/
+    templates=$(git diff --name-only ${GITHUB_SHA} HEAD~1 templates/ | tpl_path_join)
+    for template in ${templates}; do
+        if [ $DRY_RUN = "false" ]; then 
+            sendsync apply templates/${template}/template.json
+        else 
+            echo "DRY RUN: would have applied ${template}"
+        fi
+    done
+else 
+    echo "Mode ${MODE} not supported"
+    exit 1
+fi
