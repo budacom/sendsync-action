9a2c8848f7540a0123be0ca122cf12d9db24cc42
Juan Ignacio Donoso <jidonoso@gmail.com>
2021-05-11 11:26:42 -0400

    refactor: change script structure

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,103 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
-set -x
+# Copyright Buda.com.SpA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-DRY_RUN="$1"
-MODE="$2"
+set -o errexit
+set -o nounset
+set -o pipefail
 
-alias tpl_path_join="cut -d'/' -f 2 | sort | uniq"
-git config --global user.email "devops@buda.com"
-git config --global user.name "Buda CD"
-git config pull.rebase true
-git fetch origin
-git merge origin/master
+show_help() {
+cat << EOF
+Usage: $(basename "$0") <options>
+    -h, --help               Display help
+    -m, --mode               The mode in which to run the action (one of: sync, apply)
+    -d, --dry-run            Run the action with dry-run
 
-if [ $MODE = "sync" ]; then
+EOF
+}
 
+main() {
+    local mode=
+    local dry_run=false
+
+    parse_command_line "$@"
+
+    if [[ $mode = "sync" ]]; then
+        sync
+    elif [[ $mode = "apply" ]]; then
+        apply
+    fi
+
+    echo $mode
+}
+
+parse_command_line() {
+    while :; do
+        case "${1:-}" in
+            -h|--help)
+                show_help
+                exit
+                ;;
+            -v|--version)
+                if [[ -n "${2:-}" ]]; then
+                    version="$2"
+                    shift
+                else
+                    echo "ERROR: '-v|--version' cannot be empty." >&2
+                    show_help
+                    exit 1
+                fi
+                ;;
+            -d|--mode)
+                if [[ -n "${2:-}" ]]; then
+                    mode="$2"
+                    shift
+                else
+                    echo "ERROR: '-m|--mode' cannot be empty." >&2
+                    show_help
+                    exit 1
+                fi
+                ;;
+            -u|--dry-run)
+                dry_run=true
+                ;;
+            *)
+                break
+                ;;
+        esac
+
+        shift
+    done
+
+    if [[ -z "$mode" ]]; then
+        echo "ERROR: '-m|--mode' is required." >&2
+        show_help
+        exit 1
+    fi
+
+    if [[ ! "$mode" =~ ^(sync|apply)$ ]]; then
+        echo "ERROR: '$mode mode is not supported." >&2
+        show_help
+        exit 1
+    fi
+}
+
+sync() {
+    setup_git
+
+    echo "Running in syncronization mode"
     sendsync get template
 
     templates=$(git status -s templates | cut -d' ' -f3 | tpl_path_join)
@@ -21,7 +105,7 @@ if [ $MODE = "sync" ]; then
     git stash
 
     for template in $(echo ${templates}); do
-        if [ $DRY_RUN = "false" ]; then 
+        if [ $DRY_RUN = "false" ]; then
             git branch ${template}
             git checkout ${template}
             git pull origin ${template}
@@ -33,11 +117,17 @@ if [ $MODE = "sync" ]; then
             gh pr create -d --title "(auto) Publish template: ${template}." --body "Detected changes in template ${template}. Review this PR to approve."
             git stash
             git checkout master
-        else 
+        else
             echo "DRY RUN: would have created PR for ${template}"
         fi
     done
-elif [ $MODE = "apply" ]; then
+}
+
+apply() {
+    setup_git
+
+    echo "Running in apply mode"
+
     git pull --unshallow
     git diff --name-only ${GITHUB_SHA} HEAD~1 templates/
     templates=$(git diff --name-only -m ${GITHUB_SHA} HEAD~1 templates/ | tpl_path_join)
@@ -48,7 +138,16 @@ elif [ $MODE = "apply" ]; then
             echo "DRY RUN: would have applied ${template}"
         fi
     done
-else 
-    echo "Mode ${MODE} not supported"
-    exit 1
-fi
+}
+
+setup_git() {
+    alias tpl_path_join="cut -d'/' -f 2 | sort | uniq"
+    git config --global user.email "devops@buda.com"
+    git config --global user.name "Buda CD"
+    git config pull.rebase true
+    git fetch origin
+    git merge origin/master
+}
+
+
+main "$@"
